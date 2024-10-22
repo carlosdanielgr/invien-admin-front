0737c59e5e82b838e3ca6e0973f500d1bda8b6dc
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-22 13:09:07 -0500

    Login component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'invien-admin-front';
-}
+export class AppComponent {}
