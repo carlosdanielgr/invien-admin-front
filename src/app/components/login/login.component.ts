3d21df2eda0a681ea168b964a084c03248f5f60d
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-22 17:08:55 -0500

    Form login configured

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
 
+  constructor(private readonly router: Router) {}
+
+  sendLogin() {
+    localStorage.setItem('token', 'abcd1234');
+    this.router.navigate(['admin']);
+  }
 }
