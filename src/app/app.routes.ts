d58c43c974ed14a2cb89b794e96c959f32ba4e78
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-10-22 13:31:57 -0500

    Router config completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth-guard';
+import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [authGuard()],
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
