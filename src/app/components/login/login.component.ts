07f28672d45df9c17af2def537a1a0c348cdc072
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-20 12:35:08 -0500

    alert login implemented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,8 +6,10 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 import { Login, LoginService } from './login.service';
+import { errorFn } from '@shared/errors';
 
 @Component({
   selector: 'app-login',
@@ -22,18 +24,23 @@ export class LoginComponent {
     password: new FormControl('', Validators.required),
   });
 
+  loading = false;
+
   constructor(
     private readonly router: Router,
     private readonly loginService: LoginService
   ) {}
 
   sendLogin() {
+    this.loading = true;
     this.loginService.postLogin(this.form.value as Login).subscribe({
       next: (response) => {
         localStorage.setItem('token', response.token);
         this.router.navigate(['admin']);
       },
       error: (error) => {
+        Swal.fire(errorFn('Usuario o contraseña incorrecta'));
+        this.loading = false;
         console.error(error);
       },
     });
