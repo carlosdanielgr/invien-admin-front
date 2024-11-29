6ed208b54c24f0019d2374d5acdc0d325358503c
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-29 13:28:36 -0500

    fix in the interceptor redirect to login completed

--- src/app/core/admin.interceptor.ts
+++ src/app/core/admin.interceptor.ts
@@ -1,26 +1,28 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { errorFn } from '@shared/functions/errors.function';
 import { catchError, throwError } from 'rxjs';
+
+import { errorFn } from '@shared/functions/errors.function';
 
 export const adminInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
-
   const token = localStorage.getItem('token');
+
+  const handleError = (err: any) => {
+    if (err.status === 401) {
+      localStorage.removeItem('token');
+      router.navigate(['']);
+      errorFn('La sesión ha expirado');
+    }
+    return throwError(() => err);
+  };
+
   if (token) {
     const clonedReq = req.clone({
       headers: req.headers.set('Authorization', token),
     });
-    return next(clonedReq);
+    return next(clonedReq).pipe(catchError((err) => handleError(err)));
   }
-  return next(req).pipe(
-    catchError((err) => {
-      if (err.status === 401) {
-        router.navigate(['']);
-        errorFn('La sesión ha expirado');
-      }
-      return throwError(() => err);
-    })
-  );
+  return next(req).pipe(catchError((err) => handleError(err)));
 };
