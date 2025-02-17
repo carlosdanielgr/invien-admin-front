27b6813991a8fc1a2f91fb4e5e6031030cf04ae1
carlosdanielgr <carlos.dgr.12@gmail.com>
2025-02-17 16:15:19 -0500

    fix load image advisor completed

--- src/app/components/admin/pages/advisors/components/adviser/adviser.component.ts
+++ src/app/components/admin/pages/advisors/components/adviser/adviser.component.ts
@@ -9,20 +9,16 @@ import { Advisor } from '@shared/interfaces/project.interface';
   templateUrl: './adviser.component.html',
   styleUrl: './adviser.component.scss',
 })
-export class AdviserComponent implements OnInit {
+export class AdviserComponent {
   @Input() adviser!: Advisor;
 
   @Output() editAdviser = new EventEmitter<Advisor>();
 
   @Output() deleteAdviser = new EventEmitter<string>();
 
-  advisorImage = '';
+  advisorImage = `${environment.apiUrl}uploads/advisors/`;
 
   constructor() {}
-
-  ngOnInit(): void {
-    this.advisorImage = `${environment.apiUrl}uploads/advisors/${this.adviser.image}`;
-  }
 
   onDelete() {
     this.deleteAdviser.emit(this.adviser.id);
